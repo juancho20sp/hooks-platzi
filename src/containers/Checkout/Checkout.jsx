6f914427cd5c0872b7df1dafe68e873df36df29f
juancho20sp <juancho20sp@gmail.com>
2020-12-17 19:36:35 -0500

    Information done

--- src/containers/Checkout/Checkout.jsx
+++ src/containers/Checkout/Checkout.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import './Checkout.css';
 
 const Checkout = () => {
   return (
-    <div>
-      <p>Checkout</p>
+    <div className="checkout">
+      <div className="checkout-content">
+        <h3>Lista de deseos:</h3>
+
+        <div className="checkout-item">
+          <div className="checkout-element">
+            <h4>Item name</h4>
+            <span>$10</span>
+          </div>
+
+          <button type="button">Eliminar</button>
+        </div>
+      </div>
+
+      <div className="checkout-sidebar">
+        <h3>Valor total: $10</h3>
+        <button type="button">Continuar pedido</button>
+      </div>
     </div>
   );
 };
--- src/containers/Information/Information.jsx
+++ src/containers/Information/Information.jsx
@@ -1,9 +1,49 @@
 import React from 'react';
+import './Information.css';
 
 const Information = () => {
   return (
-    <div>
-      <p>Information</p>
+    <div className="information">
+      <div className="information-content">
+        <div className="information-head">
+          <h2>Información de contacto:</h2>
+        </div>
+
+        <div className="information-form">
+          <form>
+            <input type="text" placeholder="Nombre completo" name="name" />
+            <input type="text" placeholder="Correo electrónico" name="email" />
+            <input type="text" placeholder="Dirección" name="address" />
+            <input
+              type="text"
+              placeholder="Edificio, apartamento, oficina"
+              name="apto"
+            />
+            <input type="text" placeholder="Ciudad" name="city" />
+            <input type="text" placeholder="País" name="country" />
+            <input type="text" placeholder="Departamento" name="department" />
+            <input type="text" placeholder="Código postal" name="cp" />
+            <input type="text" placeholder="Teléfono" name="phone" />
+          </form>
+        </div>
+
+        <div className="information-buttons">
+          <div className="information-back">Regresar</div>
+
+          <div className="information-next">Pagar</div>
+        </div>
+      </div>
+
+      <div className="information-sidebar">
+        <h3>Pedido:</h3>
+        <div className="information-item">
+          <div className="information-element">
+            <h4>
+              Item name <span>$10</span>
+            </h4>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
